e71895d1d5140dbbba398cd949f87f317a664d90
Roy Lucas <123355936+SnowyyNL@users.noreply.github.com>
2023-01-23 11:38:20 +0100

    Delete config.lua

--- config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
--- zet hier jou webhook link.
-Config.discordwebhooklink = ''
-
-
-Config.displayidentifiers = true -- Display the user identifiers in the embed? Steam id, discord id etc
