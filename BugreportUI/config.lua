2ccbba4317eec3efe70d5ebdbe2bd20376c826e1
Roy Lucas <123355936+SnowyyNL@users.noreply.github.com>
2023-01-23 11:44:20 +0100

    Add files via upload

---
+++ BugreportUI/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+-- zet hier jou webhook link.
+Config.discordwebhooklink = ''
+
+
+Config.displayidentifiers = true -- Display the user identifiers in the embed? Steam id, discord id etc
